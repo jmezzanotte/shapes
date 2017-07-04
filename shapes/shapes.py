6081ce843ab7c6dd2fef7ed45b80940e1d6f5885
jmezzanotte <johnjmezzanotte@gmail.com>
2017-07-04 10:59:26 -0700

    Intial commit of shapes.

---
+++ shapes/shapes.py
@@ -0,0 +1,111 @@
+class Triangle(object):
+
+    SINE = "Opposite / Hypotenuse"
+    COSINE = "Adjacent / Hypotenuse"
+    TANGENT = "Opposite / Adjacent"
+
+    def __init__(self, *args, **kwargs):
+        self.args = list(args)
+        self.hypotenuse = args.pop() if len(self.args) > 0 else kwargs.get('hypotenuse', None)
+        self.adjacent = args.pop() if len(self.args) > 0 else kwargs.get('adjacent', None)
+        self.opposite = args.pop() if len(self.args) > 0 else kwargs.get('opposite', None)
+        self.triangle = [self.hypotenuse, self.adjacent, self.opposite]
+
+    @property
+    def hypotenuse(self):
+        return self._hypotenuse
+
+    @hypotenuse.setter
+    def hypotenuse(self, value):
+        try:
+            int(value)
+        except ValueError:
+            raise ValueError("Value must be an integer.")
+        if value < 0:
+            raise ValueError("Hypotenuse cannot be less than 0.")
+        self._hypotenuse = value
+
+    @property
+    def adjacent(self):
+        return self._adjacent
+
+    @adjacent.setter
+    def adjacent(self, value):
+        try:
+            int(value)
+        except ValueError:
+            raise ValueError("Value must be an integer.")
+        if value < 0:
+            raise ValueError("Adjacent side cannot be less than 0")
+        self._adjacent = value
+
+    @property
+    def opposite(self):
+        return self._opposite
+
+    @opposite.setter
+    def opposite(self, value):
+        try:
+            int(value)
+        except ValueError:
+            raise ValueError("Value must be an integer.")
+        if value < 0:
+            raise ValueError("Opposite side cannot be less than 0.")
+        self._opposite = value
+
+    def is_scalene(self):
+
+        """
+        Determines if this triangle object is scalene
+        :return: Returns True if this triangle is scalene, False otherwise
+        """
+
+        return len(list(set(self.triangle))) == 3
+
+    def is_equilateral(self):
+
+        """
+        Determines if this triangle object is equilateral.
+        :return: Returns True if this triangle is equilateral, False otherwise
+        """
+
+        return len(list(set(self.triangle))) == 1
+
+    def is_isosceles(self):
+
+        """
+        Determines if this triangle object is isosceles. 
+        :return: Returns True if this triange is isosceles, False otherwise.
+        """
+
+        return len(list(set(self.triangle))) == 2
+
+    def is_right(self):
+        """
+        Determines if this triangle object is a right triangle
+        :return: Returns True if this triangle is a right triangle, False otherwise
+        """
+        return self.adjacent**2 + self.opposite**2 == self.hypotenuse**2
+
+    @property
+    def sine(self):
+        return self.opposite / self.hypotenuse
+
+    @property
+    def cosine(self):
+        return self.adjacent / self.hypotenuse
+
+    @property
+    def tangent(self):
+        return self.opposite / self.adjacent
+
+if __name__ == "__main__":
+
+    # Quick unit tests of the module.
+    t = Triangle(hypotenuse=5, adjacent=4, opposite=3)
+    print t.is_equilateral()
+    print t.is_isosceles()
+    print t.is_scalene()
+    print t.hypotenuse
+    print t.sine
+    print "This triangle is has a right angle:{0}".format(t.is_right())
---
+++ tests/shapes_unit_test.py
@@ -0,0 +1,45 @@
+import unittest
+from shapes.shapes import Triangle
+
+
+class EquilateralTriangleTestCase(unittest.TestCase):
+
+    """
+    Tests for Triangle class in shapes.pt
+    """
+
+    def setUp(self):
+        """
+        Instantiates a Triangle object to be used for testing.
+        :return: 
+        """
+        print "Running setup"
+
+        self.test_triange = Triangle(hypotenuse=3, opposite=3, adjacent=3)
+
+    def test_is_scalene(self):
+        """
+        Tests whether or not the test object correctly identifies a scalene triangle.
+        :return: Returns True if test passes
+        """
+
+        self.assertFalse(self.test_triange.is_scalene())
+
+    def test_is_equilateral(self):
+        """
+        Tests whether or not the test object correctly identifies an equilateral triangle.
+        :return: Returns True if test passes, False otherwise
+        """
+        self.assertTrue(self.test_triange.is_equilateral())
+
+    def test_is_isosceles(self):
+        """
+        Tests whether or not the test object correctly identifies an isosceles triangle.
+        :return: 
+        """
+        self.assertFalse(self.test_triange.is_isosceles())
+
+
+if __name__ == "__main__":
+    unittest.main()
+
